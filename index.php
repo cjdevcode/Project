3ae5960925a1867f4398db43169b8146c44f2c77
celso del rio <dellricoh@outlook.com>
2019-12-09 16:35:45 -0500

    mod2

--- index.php
+++ index.php
@@ -1,147 +1,115 @@
 <?php
-require_once "../config.php";
-use \Tsugi\Core\Settings;
-use \Tsugi\Core\LTIX;
-use \Tsugi\UI\SettingsForm;
-use \Tsugi\UI\Lessons;
-$LAUNCH = LTIX::requireData();
-$p = $CFG->dbprefix;
-if ( SettingsForm::handleSettingsPost() ) {
-    header( 'Location: '.addSession('index.php') ) ;
+
+// Demand a GET parameter
+// check the $_GET variable to see if the user's name is set and if the user's name is not present
+if (!isset($_GET['name']) || strlen($_GET['name']) < 1 ) {
+    die('Name parameter missing');
+}
+
+if (strpos($_GET['name'], '@') === false ) {
+    die('Name parameter is wrong');
+}
+
+// If the user requested logout go back to index.php
+if (isset($_POST['logout']) ) {
+    header('Location: index.php');
     return;
 }
-// All the assignments we support
-$assignments = array(
-    'http_headers.php' => 'Exploring HTTP Headers',
-    'howdy.php' => 'Howdy application', 
-    'guess.php' => 'Guessing Game',
-    'rps.php' => 'Rock, Paper, Scissors',
-    'autosdb.php' => 'Autos PDO',
-    'autosess.php' => 'Autos Post-Redirect',
-    'autoscrud.php' => 'Autos CRUD',
-    'res-profile.php' => 'Profiles Database',
-    'res-position.php' => 'Profiles+Positions Database',
-    'res-education.php' => 'Prof+Pos+Educ Database',
-    'practical-f16-cats.php' => 'Fall 16 Cats Exam',
-    'practical-w17-parking.php' => 'Winter 17 Parking Exam',
-    'practical-w17-dogs.php' => 'Winter 17 Dogs Exam',
-    'practical-f17-courses.php' => 'Fall 17 Courses Exam',
-    'practical-sample.php' => 'Sample Practical Exam'
-);
-$oldsettings = Settings::linkGetAll();
-$assn = Settings::linkGet('exercise');
-$custom = LTIX::ltiCustomGet('exercise');
-if ( $assn && isset($assignments[$assn]) ) {
-    // Configured
-} else if ( strlen($custom) > 0 && isset($assignments[$custom]) ) {
-    Settings::linkSet('exercise', $custom);
-    $assn = $custom;
+
+try {
+    require_once "pdo.php";
 }
-if ( $assn === false && isset($_GET["inherit"]) && isset($CFG->lessons) ) {
-    $l = new Lessons($CFG->lessons);
-    if ( $l ) {
-        $lti = $l->getLtiByRlid($_GET['inherit']);
-        if ( isset($lti->custom) ) foreach($lti->custom as $custom ) {
-            if (isset($custom->key) && isset($custom->value) && $custom->key == 'exercise' ) {
-                $assn = $custom->value;
-                Settings::linkSet('exercise', $assn);
-            }
-        }
+catch(PDOException $e){
+    echo "Please contact Customer Support.";
+    die();
+}
+
+// resistant to HTML Injection attempts
+$name = htmlentities($_GET['name']);
+$status = false;  // If we have no POST data
+$status_color = 'red';
+
+// Check to see if we have some POST data, if we do process it
+if (isset($_POST['mileage']) && isset($_POST['year']) && isset($_POST['make'])) {
+    // If either mileage or year field is not nummeric
+    if (!is_numeric($_POST['mileage']) || !is_numeric($_POST['year']) ) {
+        $status = "Mileage and year must be numeric";
+    }
+    // if make field is empty
+    else if (strlen($_POST['make']) < 1){
+        $status = "Make is required";
+    }
+    else {
+        // resistant to HTML Injection attempts
+        $make = htmlentities($_POST['make']);
+        $year = htmlentities($_POST['year']);
+        $mileage = htmlentities($_POST['mileage']);
+        // resistant to SQL Injection attempts
+        $stmt = $pdo->prepare('
+            INSERT INTO autos (make, year, mileage) 
+            VALUES (:make, :year, :mileage)
+        ');
+
+        $stmt->execute(array(
+            ':make' => $make, 
+            ':year' => $year,
+            ':mileage' => $mileage)
+        );
+
+        $status = 'Record inserted';
+        $status_color = 'green';
     }
 }
-// Get any due date information
-$dueDate = SettingsForm::getDueDate();
-// Let the assignment handle the POST
-if ( count($_POST) > 0 && $assn && isset($assignments[$assn]) ) {
-    require($assn);
-    return;
+
+$autos = [];
+$all_autos = $pdo->query("SELECT * FROM autos");
+
+while ($row = $all_autos->fetch(PDO::FETCH_OBJ) ) {
+    $autos[] = $row;
 }
-// View
-$OUTPUT->header();
-$OUTPUT->bodyStart();
-$OUTPUT->topNav();
-// Settings button and dialog
-echo('<div style="float: right;">');
-if ( $USER->instructor ) {
-    if ( $CFG->launchactivity ) {
-        echo('<a href="analytics" class="btn btn-default">Launches</a> ');
-    }
-    echo('<a href="grades.php" target="_blank"><button class="btn btn-info">Grade detail</button></a> '."\n");
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<?php require_once "bootstrap.php"; ?>
+<title>Lee-Pin Shing's Automobiles Database</title>
+</head>
+<body>
+<div class="container">
+<h1>Tracking Autos for <?php echo $name; ?></h1>
+<?php
+// Note triple not equals and think how badly double
+// not equals would work here...
+if ( $status !== false ) {
+    // Look closely at the use of single and double quotes
+    echo('<p style="color:'.$status_color.';">'.htmlentities($status)."</p>\n");
 }
-SettingsForm::button();
-echo('</div>');
-SettingsForm::start();
-SettingsForm::select("exercise", __('Please select an assignment'),$assignments);
-SettingsForm::dueDate();
-SettingsForm::done();
-SettingsForm::end();
-$OUTPUT->flashMessages();
-$code = $USER->id+$LINK->id+$CONTEXT->id;
-if ( ! $USER->displayname || $USER->displayname == '' ) {
-    echo('<p style="color:blue;">Auto grader launched without a student name.</p>'.PHP_EOL);
-} else {
-    $OUTPUT->welcomeUserCourse();
-}
-$ALL_GOOD = false;
-function my_error_handler($errno , $errstr, $errfile, $errline , $trace = false)
-{
-    global $OUTPUT, $ALL_GOOD;
-    if ( ob_get_status() ) {
-        $ob_output = ob_get_contents();
-        ob_end_clean();
-        echo($ob_output);
-    }
-    error_out("The autograder did not find something it was looking for in your HTML - test ended.");
-    error_out("Usually the problem is in one of the pages returned from your application.");
-    error_out("Use the 'Toggle' links above to see the pages returned by your application.");
-    $message = $errfile."@".$errline." ".$errstr;
-    error_log($message);
-    if ( $trace ) error_log($trace);
-    $detail = 
-        "Check the most recently retrieved page (above) and see why the autograder is uphappy.\n" .
-        "\nHere is some internal detail where the autograder was unable to continue.\n".
-        'Caught exception: '.$message."\n".$trace."\n";
-    showHTML("Internal error detail.",$detail);
-    $OUTPUT->footer();
-    $ALL_GOOD = true;
-}
-function fatalHandler() {
-    global $ALL_GOOD, $OUTPUT;
-    if ( $ALL_GOOD ) return;
-    if ( ob_get_status() ) {
-        $ob_output = ob_get_contents();
-        ob_end_clean();
-        echo($ob_output);
-    }
-    $error = error_get_last();
-    error_out("Fatal error handler triggered");
-    if($error) {
-        my_error_handler($error["type"], $error["message"], $error["file"], $error["line"]);
-    } else {
-        $OUTPUT->footer();
-    }
-    exit();
-}
-register_shutdown_function("fatalHandler");
-// Assume try / catch is in the script
-if ( $assn && isset($assignments[$assn]) ) {
-    ob_start();
-    include($assn);
-    $ob_output = ob_get_contents();
-    ob_end_clean();
-    echo($ob_output);
-    $LAUNCH->result->setJsonKey('output', $ob_output);
-} else {
-    if ( $USER->instructor ) {
-        echo("<p>Please use settings to select an assignment for this tool.</p>\n");
-    } else {
-        echo("<p>This tool needs to be configured - please see your instructor.</p>\n");
-    }
-}
-$ALL_GOOD = true;
-if ( ob_get_status() ) {
-    $ob_output = ob_get_contents();
-    ob_end_clean();
-    echo($ob_output);
-}
-$OUTPUT->footer();
+?>
+<form method="POST">
+<label for="make">Make:</label>
+<input type="text" name="make" id="make" size="60"><br/>
+<br/>
+<label for="year">Year:</label>
+<input type="text" name="year" id="year"><br/>
+<br/>
+<label for="mileage">Mileage:</label>
+<input type="text" name="mileage" id="mileage"><br/>
+<br/>
+<input type="submit" value="Add">
+<input type="submit" name="logout" value="Logout">
+</form>
+
+<h2>Automobiles</h2>
+<?php if(!empty($autos)) : ?>
+<ul>
+<?php foreach($autos as $auto) : ?>
+<li>
+<?php echo $auto->year; ?> <?php echo $auto->make; ?> &#47; <?php echo $auto->mileage; ?> 
+</li>
+<?php endforeach; ?>
+</ul>
+<?php endif; ?>
+</div>
+</body>
+</html>
