817b91761bc72643111a4abc4c781b12781bf211
CJ <dellricoh@outlook.com>
2021-08-05 02:41:12 -0500

    add forEach function

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 const tabs = document.querySelectorAll('#tabs > ul > li > a');
-for( let i = 0; i < tabs.length; i++){
+/* for( let i = 0; i < tabs.length; i++){
     tabs[i].addEventListener('click', selTab);
-}
+} */
+tabs.forEach(function(n){n.addEventListener('click', selTab)});
 
 function selTab(e){
     e.preventDefault();
-    for (let i = 0; i < tabs.length; i++){tabs[i].removeAttribute('class');}
+    /* for (let i = 0; i < tabs.length; i++){tabs[i].removeAttribute('class');} */
+    tabs.forEach(function(x){x.removeAttribute('class');});
     e.target.className = 'active';
     const attr = e.target.getAttribute('href');
     const cont = document.querySelector(attr);
