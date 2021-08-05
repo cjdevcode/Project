88a20e0960ca9f40d2aa2c33e09620b65f399b41
CJ <dellricoh@outlook.com>
2021-08-05 02:50:30 -0500

    minor change

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-const tabs = document.querySelectorAll('#tabs > ul > li > a');
-/* for( let i = 0; i < tabs.length; i++){
-    tabs[i].addEventListener('click', selTab);
-} */
+(function () {
+    "use strict";
+    const tabs = document.querySelectorAll('#tabs > ul > li > a');
+    tabs.forEach(tab => { tab.addEventListener('click', selectTab); });
+    function selectTab(event) {
+        event.preventDefault();
+        tabs.forEach(tab => { tab.removeAttribute('class'); });
+        event.target.className = 'active';
+        const thisTab = event.target.getAttribute('href');
+        const thisContent = document.querySelector(thisTab);
+        const oldTabContent = document.querySelector('.visible');
+        oldTabContent.className = 'visuallyhidden';
+        oldTabContent.addEventListener('transitionend', () => {
+            oldTabContent.className = 'hidden';
+            thisContent.className = 'visible visuallyhidden';
+            setTimeout(() => {
+                thisContent.classList.remove('visuallyhidden');
+            }, 20);
+        }, { capture: false, once: true, passive: false });
+    }
+}());
+/* const tabs = document.querySelectorAll('#tabs > ul > li > a');
+
 tabs.forEach(function(n){n.addEventListener('click', selTab)});
 
 function selTab(e){
     e.preventDefault();
-    /* for (let i = 0; i < tabs.length; i++){tabs[i].removeAttribute('class');} */
     tabs.forEach(function(x){x.removeAttribute('class');});
     e.target.className = 'active';
     const attr = e.target.getAttribute('href');
@@ -18,5 +36,5 @@ function selTab(e){
         cont.className = 'active visuallyhidden';
         setTimeout(function(){cont.classList.remove('visuallyhidden')}, 20);
     });
-    
-}
+
+} */
