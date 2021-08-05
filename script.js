5dfed756dcb91685279f062513e41c2edb0e933b
CJ <dellricoh@outlook.com>
2021-08-05 02:28:42 -0500

    minor change

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const tabs = document.querySelectorAll('#tabs > ul > li > a');
 for( let i = 0; i < tabs.length; i++){
-    tabs[i].addEventListener('click', selTab)
+    tabs[i].addEventListener('click', selTab);
 }
 
 function selTab(e){
     e.preventDefault();
     for (let i = 0; i < tabs.length; i++){tabs[i].removeAttribute('class');}
     e.target.className = 'active';
+    const attr = e.target.getAttribute('href');
+    const cont = document.querySelector(attr);
+    const old = document.querySelector('.visible');
+    old.className = 'visuallyhidden';
+    old.addEventListener('transitionend', function() {
+        old.className = 'hidden';
+        cont.className = 'active visuallyhidden';
+        setTimeout(function(){cont.classList.remove('visuallyhidden')}, 20);
+    });
+    
 }
