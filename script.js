3cb4c54abc2a38486b2356c4649583139f967937
CJ <dellricoh@outlook.com>
2021-08-09 12:18:34 -0500

    minor change

--- script.js
+++ script.js
@@ -3,20 +3,20 @@ const dv = document.getElementById('div');
 /* setTimeout(function(){dv.setAttribute('class', 'two')},2000); */
 /* setTimeout(() => dv.setAttribute('class', 'two'), 1000); */
 let cur = 'one';
-function rotator(){
-    if(cur === 'one'){
-        setTimeout(function(){
+function rotator() {
+    if (cur === 'one') {
+        setTimeout(function () {
             dv.className = 'two';
             cur = 'two';
             rotator();
         }, 2000);
     } else {
-        setTimeout(function(){
+        setTimeout(function () {
             dv.className = 'one';
             cur = 'one';
             rotator();
         }, 2000);
-        
+
     }
 }
 rotator();
