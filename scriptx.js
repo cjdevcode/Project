b5eed749c3fe26dbcce533e352f583198adab9f5
CJ <dellricoh@outlook.com>
2021-08-09 00:12:45 -0500

    complete challenge

--- scriptx.js
+++ scriptx.js
@@ -1,1 +1,27 @@
 
+window.addEventListener('load', function () {
+    const sldcont = document.querySelectorAll('#slider-wrapper ul li').length;
+    const sldwidth = document.querySelector('#slider-wrapper').offsetWidth;
+    const total = sldcont * sldwidth + 'px';
+    const slider = document.querySelector('#slider-wrapper ul');
+    const nxt = document.querySelector('#next');
+    const pre = document.querySelector('#prev');
+    let count = 0;
+    let leftpos = 0;
+    slider.style.width = total;
+    nxt.addEventListener('click', function (e) {
+        e.preventDefault();
+        count++
+        if (count === sldcont) { count = 0; }
+        leftpos = `-${sldwidth * count}px`;
+        slider.style.left = leftpos;
+    });
+    pre.addEventListener('click', function (e) {
+        e.preventDefault();
+        count--
+        if (count < 0) { count = sldcont - 1 }
+        leftpos = `-${sldwidth * count}px`;
+        slider.style.left = leftpos;
+    });
+
+});
