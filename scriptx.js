d7fb8831e44374d7bd84e9dfeccfbe9018942683
CJ <dellricoh@outlook.com>
2021-08-10 12:52:05 -0500

    continue recursion

--- scriptx.js
+++ scriptx.js
@@ -1,27 +1,26 @@
+(function () {
+    'use strict';
+    let counter = 1;
+    function contentRotator(){
+        $(`#container p:nth-child(${counter})`).fadeIn(2000, function(){
+            if ($(this).is('#container p:last-child')){
+                setTimeout(function(){
+                    $(`#container p:nth-child(${counter})`).fadeOut(2000, function(){
+                        counter = 1;
+                        contentRotator();
+                    });
+                }, 7000);
 
-window.addEventListener('load', function () {
-    const sldcont = document.querySelectorAll('#slider-wrapper ul li').length;
-    const sldwidth = document.querySelector('#slider-wrapper').offsetWidth;
-    const total = sldcont * sldwidth + 'px';
-    const slider = document.querySelector('#slider-wrapper ul');
-    const nxt = document.querySelector('#next');
-    const pre = document.querySelector('#prev');
-    let count = 0;
-    let leftpos = 0;
-    slider.style.width = total;
-    nxt.addEventListener('click', function (e) {
-        e.preventDefault();
-        count++
-        if (count === sldcont) { count = 0; }
-        leftpos = `-${sldwidth * count}px`;
-        slider.style.left = leftpos;
-    });
-    pre.addEventListener('click', function (e) {
-        e.preventDefault();
-        count--;
-        if (count < 0) { count = sldcont - 1 }
-        leftpos = `-${sldwidth * count}px`;
-        slider.style.left = leftpos;
-    });
+            } else {
+                setTimeout(function(){
+                    $(`#container p:nth-child(${counter})`).fadeOut(2000, function(){
+                        counter++;
+                        contentRotator();
+                    });
+                }, 3000);
+            }
+        });
+    }
 
-});
+    contentRotator();
+})();
